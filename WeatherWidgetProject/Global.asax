c67ae1a8dbfa72a3489b0e27a949ef636ca0165d
Arx <Arximiro@outlook.com>
2017-09-25 13:53:04 -0400

    Add project files.

---
+++ WeatherWidgetProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeatherWidgetProject.MvcApplication" Language="C#" %>
---
+++ WeatherWidgetProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeatherWidgetProject.Startup))]
+namespace WeatherWidgetProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
